d2ab2fdb7f8a71c3d235aade0b441a95d739927b
alfredo <alfredo.rizzi@psmobile.it>
2017-03-02 07:16:53 +0100

    Prima versione

---
+++ Lucine/LucinePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using MvvmCross.Platform;
+using Plugin.BLE.Abstractions.Contracts;
+using Xamarin.Forms;
+
+namespace Lucine
+{
+	public partial class LucinePage : ContentPage
+	{
+		IAdapter adapter;
+		public LucinePage ()
+		{
+			InitializeComponent ();
+		}
+
+		List<IDevice> deviceList = new List<IDevice> ();
+		protected override async void OnAppearing ()
+		{
+			base.OnAppearing ();
+			adapter = Mvx.Resolve<IAdapter> ();
+			adapter.DeviceDiscovered += (s, a) => deviceList.Add (a.Device);
+			await adapter.StartScanningForDevicesAsync ();
+			foreach (var d in deviceList)
+				System.Diagnostics.Debug.WriteLine ($"GUID: {d.Id} e name {d.Name}");
+		}
+	}
+}
